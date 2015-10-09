659217984542cc971148428a3834fd1bf4e7c624
lenzenc <lenzenc@gmail.com>
2015-10-08 19:48:32 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scala Validations
+----
+Scala Validations is a pure Scala validation framework that is/was largely inspired by the ActiveRecord::Validations framework in Ruby on Rails.  The intent of this framework is to be flexible enough to be used in many different use cases given a typical SOA type service stack.  Providing controller level parameter validation to service interface and domain model validations. 
---
+++ project/build.scala
@@ -0,0 +1,34 @@
+import sbt._
+import Keys._
+
+
+object ScalaValidations extends Build {
+
+  lazy val _scalacOptions = Seq("-deprecation", "-unchecked", "-feature")
+
+  lazy val commonSettings = Seq(
+    version := "1.0",
+    organization := "com.payit",
+    scalaVersion := "2.11.4",
+    ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) },
+    resolvers ++= Seq(
+      "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases",
+      "Sonatype Releases"  at "http://oss.sonatype.org/content/repositories/releases",
+      "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+    ),
+    scalacOptions ++= _scalacOptions,
+    scalacOptions ++= Seq("-unchecked", "-deprecation", "-encoding", "utf8", "-feature"),
+    scalacOptions in Test ++= Seq("-Yrangepos")
+  )
+
+  lazy val profile = Project(id = "scala-validations", base = file("."),
+    settings = commonSettings ++ Seq(
+      name := "scala-validations",
+      libraryDependencies ++= Seq(
+        "org.specs2" %% "specs2-core" % "3.6.4" % "test",
+        "org.specs2" % "specs2-mock_2.11" % "3.6.4-20150928232002-999c515" % "test"
+      )
+    )
+  )
+
+}
